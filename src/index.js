26f22b01762e5dbd85089be8b820125f5241d29f
Moustapha SOW <mousow123@gmail.com>
2024-12-08 21:49:41 +0000

    add the rest of the homepage inside the div#content, and configure the webpack config file to bundle the image in the homepage

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import "./styles.css"
+import heroImage from "./hero.png"
+
+const ImageContainer = document.querySelector(".image-container");
+
+const image = document.createElement("img");
+image.src = heroImage;
+
+console.log(ImageContainer);
+ImageContainer.appendChild(image)
 
 console.log("Restaurant page");
